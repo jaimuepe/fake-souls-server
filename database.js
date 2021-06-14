c37301311806cda3fc8e1bee5058a255aeb47f60
Jaime Muela Pérez <jaimuepe@gmail.com>
2021-06-14 20:36:30 +0200

    first commit

---
+++ database.js
@@ -0,0 +1,82 @@
+import { createPool } from "mysql2/promise";
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  database: "game",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const max_messages = 10;
+const max_message_distance = 10.0;
+const max_sqr_message_distance = max_message_distance * max_message_distance;
+
+async function create_message(data) {
+
+  const user_id = data.user_id;
+  const message = data.content;
+
+  const x = data.pos_x;
+  const y = data.pos_y;
+  const z = data.pos_z;
+
+  const query = `
+    INSERT INTO messages 
+    (user_id, content, pos_x, pos_y, pos_z)
+    VALUES
+    (?, ?, ?, ?, ?)
+  `;
+
+  await pool.execute(query, [user_id, message, x, y, z]);
+}
+
+async function get_nearby_messages(data) {
+  const user_id = data.user_id;
+
+  const x = data.pos_x;
+  const y = data.pos_y;
+  const z = data.pos_z;
+
+  const query = `
+  SELECT 
+      m.id as message_id,
+      m.pos_x as x,
+      m.pos_y as y,
+      m.pos_z as z,
+      (m.pos_x - ${x}) * (m.pos_x - ${x}) +
+        (m.pos_y - ${y}) * (m.pos_y - ${y}) +
+        (m.pos_z - ${z}) * (m.pos_z - ${z}) 
+      AS distance
+  FROM messages m WHERE  m.user_id != ${user_id}
+  HAVING distance < ${max_sqr_message_distance}
+  ORDER BY distance DESC
+  LIMIT ${max_messages}
+  `;
+
+  const [rows, _] = await pool.query(query);
+  return rows;
+}
+
+async function get_message_data(message_id) {
+  const query = `
+    SELECT
+      m.id as message_id,
+      m.content as message_content,
+      u.id as user_id,
+      u.display_name as user_display_name
+    FROM messages m 
+    INNER JOIN users u ON m.user_id = u.id 
+    WHERE m.id = ?
+  `;
+
+  const [rows, _] = await pool.query(query, [message_id]);
+  return rows.length == 1 ? rows[0] : {};
+}
+
+export default {
+  get_nearby_messages,
+  get_message_data,
+  create_message,
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+import express, { json } from "express";
+import db from "./database.js";
+
+const app = express();
+app.use(json());
+
+app.get("/message/:message_id", async (req, res) => {
+  var message_id = +req.params.message_id;
+  const data = await db.get_message_data(message_id);
+  res.json(data);
+});
+
+app.put("/message", async (req, res) => {
+
+  const body = req.body;
+
+  const user_id = +body.user_id;
+  const content = body.content;
+
+  const x = +body.pos.x;
+  const y = +body.pos.y;
+  const z = +body.pos.z;
+
+  await db.create_message({
+    user_id: user_id, content: content, pos_x: x, pos_y: y, pos_z: z
+  })
+
+  res.sendStatus(200);
+});
+
+app.post("/nearby_messages/:user_id", async (req, res) => {
+
+  const user_id = +req.params.user_id;
+
+  const body = req.body;
+  const x = +body.pos_x;
+  const y = +body.pos_y;
+  const z = +body.pos_z;
+
+  const data = await db.get_nearby_messages({
+    user_id: user_id,
+    pos_x: x,
+    pos_y: y,
+    pos_z: z,
+  });
+
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("El servidor está inicializado en el puerto 3000");
+});
